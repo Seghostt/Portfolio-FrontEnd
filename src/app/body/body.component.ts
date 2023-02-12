07c7dedd5b8a19e7caf8024257697c669c154fd2
NiceAhri <zeta.dboo@gmail.com>
2023-02-12 08:52:23 -0300

    Update Text Area Modification (Personal & Ability)

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -2,19 +2,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators} from '@angular/forms';
 
+export interface LoginInfo 
+{
+  success: boolean;
+  textMod1: string;
+  textMod2: string;
+}
+
+
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
+
 export class BodyComponent implements OnInit {
 
   constructor(private http:HttpClient) { }
   
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
     this.registerVar = document.getElementById("Register")
     this.loginVar = document.getElementById("Login")
     this.loggedinvar = document.getElementById("loggedin")
+
+    this.text_mod1 = document.getElementById("textMod1");
+    this.text_mod2 = document.getElementById("textMod2");
   }
 
   private registerVar: any
@@ -22,127 +35,196 @@ export class BodyComponent implements OnInit {
   private loggedinvar: any
   private username:any
   private password:any
-  public registerSwap(){
+
+  private text_mod1: HTMLElement | null = null;
+  private text_mod2: HTMLElement | null = null;
+
+  public registerSwap()
+  {
     this.loginVar?.setAttribute("hidden","")
     this.registerVar?.removeAttribute("hidden")
-
   }
-  public loginSwap(){
+  public loginSwap()
+  {
     this.registerVar?.setAttribute("hidden","")
     this.loginVar?.removeAttribute("hidden")
   }
 
-  public logOut(){
+  public logOut()
+  {
     this.loginVar.removeAttribute("hidden")
     this.loggedinvar.setAttribute("hidden","")
     document.getElementById("Social-icons")?.removeAttribute("hidden")
     this.removePen()
     this.removeDelete();
-    document.getElementById("textMod")?.setAttribute("readonly","true")
-    document.getElementById("textMod2")?.setAttribute("readonly","true")
+
+    if(this.text_mod1)
+      this.text_mod1.setAttribute("readonly", "true");
+
+    if(this.text_mod2)
+      this.text_mod2.setAttribute("readonly", "true");
+  
     document.getElementById("deleteB")?.setAttribute("hidden","")
   }
 
-  public PenClick(){
-    document.getElementById("textMod")?.removeAttribute("disabled")
+  public PenClick()
+  {
+    if(this.text_mod1 == null)
+      return;
+    
+    this.text_mod1.removeAttribute("readonly");
+    this.text_mod1.removeAttribute("disabled");
   }
   
-  public PenClick2(){
-    document.getElementById("textMod2")?.removeAttribute("disabled")
+  public PenClick2()
+  {
+    if(this.text_mod2 == null)
+      return;
+    
+    this.text_mod2.removeAttribute("readonly");
+    this.text_mod2.removeAttribute("disabled");
   }
-  public PenClick3(){
-  }
-
-  public sendClick(){
-    document.getElementById("textMod")?.setAttribute("disabled","")
-    this.ngSubmitText()
-  }
-
-  public sendClick2(){
-    document.getElementById("textMod2")?.setAttribute("disabled","")
-    this.ngSubmitText()
+  public PenClick3()
+  {
   }
 
-  public addPen(){
+  public sendClick()
+  {
+    if(this.text_mod1 == null)
+      return;
+
+    this.text_mod1.setAttribute("disabled", "");
+    this.ngSubmitText(0, (this.text_mod1 as HTMLInputElement).value);
+  }
+
+  public sendClick2()
+  {
+    if(this.text_mod2 == null)
+      return;
+
+    this.text_mod2.setAttribute("disabled", "");
+    this.ngSubmitText(1, (this.text_mod2 as HTMLInputElement).value);
+  }
+
+  public addPen()
+  {
     var pens = document.getElementsByClassName("pen")
-    for ( let i = 0; i< pens.length;i++){
+    for ( let i = 0; i< pens.length;i++)
+    {
       pens[i].removeAttribute("hidden")
     }
   }
   
-  addDelete(){
+  addDelete()
+  {
     var deletes = document.getElementsByClassName("deletes")
-    for ( let i = 0; i< deletes.length;i++){
+    for ( let i = 0; i< deletes.length;i++)
+    {
       deletes[i].removeAttribute("hidden")
     }
   }
 
-  removeDelete(){
+  removeDelete()
+  {
     var deletes = document.getElementsByClassName("deletes")
-    for ( let i = 0; i< deletes.length;i++){
+    for ( let i = 0; i< deletes.length;i++)
+    {
       deletes[i].setAttribute("hidden","")
     }
   }
-  deleteObject1(){
+  deleteObject1()
+  {
     document.getElementById("content-H")?.setAttribute("hidden","")
   }
-  deleteObject2(){
-    
+  deleteObject2()
+  {
     document.getElementById("about-me1")?.setAttribute("hidden","")
   }
-  deleteObject3(){
-    
+  deleteObject3()
+  {
     document.getElementById("about-me2")?.setAttribute("hidden","")
   }
 
-  public removePen(){
+  public removePen()
+  {
     var pens = document.getElementsByClassName("pen")
-    for ( let i = 0; i< pens.length;i++){
+    for ( let i = 0; i< pens.length;i++)
+    {
       pens[i].setAttribute("hidden","")
     }
   }
 
+  public UpdateText(text_field: HTMLElement | null, text: string)
+  {
+    if(text_field == null)
+      return;
 
-  public ngLogin(){//modificacion de texto con base de datos y poder eliminar
+    text_field.removeAttribute("disabled");
+    text_field.textContent = text;
+    text_field.setAttribute("disabled", "");
+  }
+
+  public ngLogin()
+  {//modificacion de texto con base de datos y poder eliminar
     if(!this.loginForm.value.password || !this.loginForm.value.username){return}
-    this.http.post("http://localhost:8080/Login",{"username":this.loginForm.value.username,"password":this.loginForm.value.password}).subscribe((res) => 
+    this.http.post<LoginInfo>("http://localhost:8080/Login",{"username":this.loginForm.value.username,"password":this.loginForm.value.password}).subscribe((res) => 
     {
-
-      if(res){
+      if(res.success)
+      {
         this.registerVar?.setAttribute("hidden","")
         this.loginVar?.setAttribute("hidden","")
         this.loggedinvar.removeAttribute("hidden")
         document.getElementById("deleteB")?.removeAttribute("hidden")
         var welcome = document.getElementById("bienvenida")
-        if(welcome !=null){
-          welcome.innerText = "Bienvenido " + this.loginForm.value.username
+        if(welcome != null)
+        {
+          welcome.innerText = "Bienvenido " + this.loginForm.value.username;
+          if(this.text_mod1)
+            this.text_mod1.textContent = res.textMod1;
+
+          if(this.text_mod2)
+            this.text_mod2.textContent = res.textMod2;
         }
         this.addPen()
         this.addDelete();
         document.getElementById("Social-icons")?.setAttribute("hidden","")
       }
+      else
+      {
+        alert("username/password incorrect!");
+      }
       this.username = this.loginForm.value.username
       this.password = this.loginForm.value.password
       this.loginForm.reset();
-    })  
+    })
   }
 
-  public ngRegister(){
-    this.http.post("http://localhost:8080/Register",{"username":this.loginForm.value.username,"password":this.loginForm.value.password}).subscribe((res) => 
+  public ngRegister()
+  {
+    this.http.post("http://localhost:8080/Register",{ "username": this.loginForm.value.username, "password": this.loginForm.value.password, "textMod1": "", "textMod2": "" }).subscribe((res) => 
     {
       this.loginForm.reset();
-      
-    })  
+      if(!res)
+      {
+        alert("User already exists!");
+      }
+      else
+      {
+        alert("User registered\n");
+        this.loginSwap();
+      }
+    })
   }
 
-
-  public ngSubmitText(){
-    this.http.post("http://localhost:8080/SaveChanges",{"username":this.username,"password":this.password,"Textmod":document.getElementById("textMod")?.textContent,"Textmod2":document.getElementById("TextMod2")?.textContent}).subscribe((res) => 
+  public ngSubmitText(index: number, text: string)
+  {
+    this.http.post("http://localhost:8080/SaveChanges",{ "username": this.username, "index": index, "text": text }).subscribe((res) => 
     {
-      console.log(document.getElementById("textMod")?.textContent)
-    })  
+      if(res)
+      {
+         alert("Text Update Success");
+      }
+    })
   }
-
-  loginForm = new FormGroup   ({       username: new FormControl('', Validators.required),       password: new FormControl('', Validators.required)   })
-  textForm = new FormGroup ({textMod : new FormControl(''), textMod2 : new FormControl('')})
+  loginForm = new FormGroup ({ username: new FormControl('', Validators.required), password: new FormControl('', Validators.required)})
 }
